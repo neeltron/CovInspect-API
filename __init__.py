73f9b292a7d12a5d898d3adc5b5ae6b76117e9aa
Neel Adwani <nprosofficial@gmail.com>
2021-05-09 17:29:22 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return ":) The data entry point is /entrypoint and exit point is /exitpoint"
+    return "Welcome to my API >:)"
 
-@app.route('/entrypoint', methods=['GET'])
+@app.route('/input', methods=['GET'])
 def entrypoint():
     ir=request.args.get("ir")
     hr=request.args.get("hr")
     output=databaseentry(ir, hr)
     return make_response(jsonify(output), 200)
 
-@app.route('/exitpoint', methods=['GET'])
+@app.route('/output', methods=['GET'])
 def exitpoint():
     output=databaseread()
     return make_response(jsonify(output), 200)
